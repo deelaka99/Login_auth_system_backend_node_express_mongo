298e20fa92af5344af6acda827f72c9c569a3b2f
deelaka99 <123wejith@gmail.com>
2024-11-08 18:00:24 +0530

    added changes to the user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const joi = require("joi");
+const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
@@ -19,15 +19,14 @@ userSchema.methods.generateAuthToken = function () {
 
 const User = mongoose.model("user", userSchema);
 
-const validate = (data)=>{
-    const schema = joi.object({
-        firstName:joi.string().required().label("First Name"),
-        lastName:joi.string().required().label("Last Name"),
-        email:joi.string().email().required().label("Email"),
-        password:passwordComplexity().required().label("Password"),
-    });
-    return schema.validate(data)
+const validate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+  });
+  return schema.validate(data);
 };
 
-module.exports = {User, validate}
-
+module.exports = { User, validate };
