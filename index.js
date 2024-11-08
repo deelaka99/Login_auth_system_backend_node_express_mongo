94d911a7a39a1189ebf4dbff144facb5f52908a6
deelaka99 <123wejith@gmail.com>
2024-11-08 18:01:56 +0530

    added changes to index.js

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 
-//database connection
+// database connection
 connection();
 
-//middlewares
+// middlewares
 app.use(express.json());
 app.use(cors());
 
-//routes
+// routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, console.log(`Listening on port ${port}...`));
